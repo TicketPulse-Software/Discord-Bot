68928c08146a24f272b7127b1e77a4d7cfcd5f71
Luc <67204364+YEET200@users.noreply.github.com>
2024-07-12 02:10:15 -0400

    Update unban.js
    Signed-off-by: Luc <67204364+YEET200@users.noreply.github.com>

--- commands/admin/unban.js
+++ commands/admin/unban.js
@@ -1,18 +1,26 @@
-module.exports = {
-  name: 'unban',
-  description: 'Unban a user from the server',
-  async execute(message, args) {
-    if (!message.member.permissions.has(PermissionsBitField.Flags.BanMembers)) return message.reply('You do not have permission to use this command.');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
-    const userId = args[0];
-    if (!userId) return message.reply('You need to provide the ID of the user to unban.');
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('unban')
+    .setDescription('Unban a user from the server')
+    .addStringOption(option =>
+      option.setName('userid')
+        .setDescription('The ID of the user to unban')
+        .setRequired(true)),
+  async execute(interaction) {
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) return interaction.reply('You do not have permission to use this command.');
+
+    const userId = interaction.options.getString('userid');
+    if (!userId) return interaction.reply('You need to provide the ID of the user to unban.');
 
     try {
-      await message.guild.members.unban(userId);
-      message.channel.send(`User with ID ${userId} has been unbanned.`);
+      await interaction.guild.members.unban(userId);
+      interaction.reply(`User with ID ${userId} has been unbanned.`);
     } catch (error) {
       console.error(error);
-      message.reply('I was unable to unban the member. Please make sure the ID is correct.');
+      interaction.reply('I was unable to unban the member. Please make sure the ID is correct.');
     }
   }
 };
+
