d0abb7f89f7a09ea3b21927375858cb4129c4aa8
Luc <67204364+YEET200@users.noreply.github.com>
2024-07-12 02:10:39 -0400

    Update unmute.js
    Signed-off-by: Luc <67204364+YEET200@users.noreply.github.com>

--- commands/admin/unmute.js
+++ commands/admin/unmute.js
@@ -1,21 +1,29 @@
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+
 module.exports = {
-  name: 'unmute',
-  description: 'Unmute a user in the server',
-  async execute(message, args) {
-    if (!message.member.permissions.has(PermissionsBitField.Flags.MuteMembers)) return message.reply('You do not have permission to use this command.');
+  data: new SlashCommandBuilder()
+    .setName('unmute')
+    .setDescription('Unmute a user in the server')
+    .addUserOption(option =>
+      option.setName('target')
+        .setDescription('The user to unmute')
+        .setRequired(true)),
+  async execute(interaction) {
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.MuteMembers)) return interaction.reply('You do not have permission to use this command.');
 
-    const member = message.mentions.members.first();
-    if (!member) return message.reply('You need to mention a user to unmute.');
+    const member = interaction.options.getMember('target');
+    if (!member) return interaction.reply('You need to mention a user to unmute.');
 
-    const muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
-    if (!muteRole) return message.reply('Mute role not found. Please create a role named "Muted".');
+    const muteRole = interaction.guild.roles.cache.find(role => role.name === 'Muted');
+    if (!muteRole) return interaction.reply('Mute role not found. Please create a role named "Muted".');
 
     try {
       await member.roles.remove(muteRole);
-      message.channel.send(`${member.user.tag} has been unmuted.`);
+      interaction.reply(`${member.user.tag} has been unmuted.`);
     } catch (error) {
       console.error(error);
-      message.reply('I was unable to unmute the member.');
+      interaction.reply('I was unable to unmute the member.');
     }
   }
 };
+
