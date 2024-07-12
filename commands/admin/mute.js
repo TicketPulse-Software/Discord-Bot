346defbd05677e14f85861152207bc9997b282ba
Luc <67204364+YEET200@users.noreply.github.com>
2024-07-12 02:09:48 -0400

    Update mute.js
    Signed-off-by: Luc <67204364+YEET200@users.noreply.github.com>

--- commands/admin/mute.js
+++ commands/admin/mute.js
@@ -1,16 +1,23 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
-  name: 'mute',
-  description: 'Mute a user in the server',
-  async execute(message, args) {
-    if (!message.member.permissions.has(PermissionsBitField.Flags.MuteMembers)) return message.reply('You do not have permission to use this command.');
+  data: new SlashCommandBuilder()
+    .setName('mute')
+    .setDescription('Mute a user in the server')
+    .addUserOption(option =>
+      option.setName('target')
+        .setDescription('The user to mute')
+        .setRequired(true)),
+  async execute(interaction) {
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.MuteMembers)) {
+      return interaction.reply('You do not have permission to use this command.');
+    }
 
-    const member = message.mentions.members.first();
-    if (!member) return message.reply('You need to mention a user to mute.');
+    const member = interaction.options.getMember('target');
+    if (!member) return interaction.reply('You need to mention a user to mute.');
 
-    const muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
-    if (!muteRole) return message.reply('Mute role not found. Please create a role named "Muted".');
+    const muteRole = interaction.guild.roles.cache.find(role => role.name === 'Muted');
+    if (!muteRole) return interaction.reply('Mute role not found. Please create a role named "Muted".');
 
     const row = new ActionRowBuilder()
       .addComponents(
@@ -24,27 +31,28 @@ module.exports = {
           .setStyle(ButtonStyle.Secondary),
       );
 
-    const muteMessage = await message.channel.send({
+    await interaction.reply({
       content: `Are you sure you want to mute ${member.user.tag}?`,
       components: [row],
     });
 
-    const filter = (interaction) => interaction.user.id === message.author.id;
-    const collector = muteMessage.createMessageComponentCollector({ componentType: 'BUTTON', time: 15000 });
+    const filter = i => i.user.id === interaction.user.id;
+    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
 
-    collector.on('collect', async (interaction) => {
-      if (interaction.customId === 'confirm-mute') {
+    collector.on('collect', async i => {
+      if (i.customId === 'confirm-mute') {
         await member.roles.add(muteRole);
-        await interaction.update({ content: `${member.user.tag} has been muted.`, components: [] });
+        await i.update({ content: `${member.user.tag} has been muted.`, components: [] });
       } else {
-        await interaction.update({ content: 'Mute action cancelled.', components: [] });
+        await i.update({ content: 'Mute action cancelled.', components: [] });
       }
     });
 
-    collector.on('end', (collected) => {
+    collector.on('end', collected => {
       if (collected.size === 0) {
-        muteMessage.edit({ content: 'Mute action timed out.', components: [] });
+        interaction.editReply({ content: 'Mute action timed out.', components: [] });
       }
     });
   }
 };
+
