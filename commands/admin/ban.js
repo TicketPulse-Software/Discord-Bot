b1c5f789295c8393aa645b26a5ef6e3591cd4d08
Luc <67204364+YEET200@users.noreply.github.com>
2024-07-12 02:09:06 -0400

    Update ban.js
    Signed-off-by: Luc <67204364+YEET200@users.noreply.github.com>

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -1,15 +1,20 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
-  name: 'ban',
-  description: 'Ban a user from the server',
-  async execute(message, args) {
-    if (!message.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-      return message.reply('You do not have permission to use this command.');
+  data: new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Ban a user from the server')
+    .addUserOption(option =>
+      option.setName('target')
+        .setDescription('The user to ban')
+        .setRequired(true)),
+  async execute(interaction) {
+    if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
+      return interaction.reply('You do not have permission to use this command.');
     }
 
-    const member = message.mentions.members.first();
-    if (!member) return message.reply('You need to mention a user to ban.');
+    const member = interaction.options.getMember('target');
+    if (!member) return interaction.reply('You need to mention a user to ban.');
 
     const row = new ActionRowBuilder()
       .addComponents(
@@ -23,26 +28,31 @@ module.exports = {
           .setStyle(ButtonStyle.Secondary),
       );
 
-    const banMessage = await message.channel.send({
+    await interaction.reply({
       content: `Are you sure you want to ban ${member.user.tag}?`,
       components: [row],
     });
 
-    const filter = (interaction) => interaction.user.id === message.author.id;
-    const collector = banMessage.createMessageComponentCollector({ componentType: 'BUTTON', time: 15000 });
+    const filter = i => i.user.id === interaction.user.id;
+    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
 
-    collector.on('collect', async (interaction) => {
-      if (interaction.customId === 'confirm-ban') {
+    collector.on('collect', async i => {
+      if (i.customId === 'confirm-ban') {
         await member.ban();
-        await interaction.update({ content: `${member.user.tag} has been banned.`, components: [] });
+        await i.update({ content: `${member.user.tag} has been banned.`, components: [] });
       } else {
-        await interaction.update({ content: 'Ban action cancelled.', components: [] });
+        await i.update({ content: 'Ban action cancelled.', components: [] });
       }
     });
 
-    collector.on('end', (collected) => {
+    collector.on('end', collected => {
       if (collected.size === 0) {
-        banMessage.edit({ content: 'Ban action timed out.', components: [] });
+        interaction.editReply({ content: 'Ban action timed out.', components: [] });
+      }
+    });
+  }
+};
+
       }
     });
   }
