4c104573556408fe7dd1bd4ff6e8aac6ad64506c
Luc <67204364+YEET200@users.noreply.github.com>
2024-07-14 19:57:42 -0400

    Update ban.js
    Signed-off-by: Luc <67204364+YEET200@users.noreply.github.com>

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -1,59 +1,55 @@
-const { SlashCommandBuilder, PermissionsBitField, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('ban')
-    .setDescription('Ban a user from the server')
-    .addUserOption(option =>
-      option.setName('target')
-        .setDescription('The user to ban')
-        .setRequired(true)),
-  async execute(interaction) {
-    if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-      return interaction.reply('You do not have permission to use this command.');
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Ban a user.')
+        .addUserOption(option => option.setName('target').setDescription('The user to ban').setRequired(true)),
+    async execute(interaction) {
+        const target = interaction.options.getUser('target');
+        const member = interaction.guild.members.cache.get(target.id);
+
+        if (interaction.member.roles.cache.some(role => role.name === 'Admin')) {
+            const embed = new MessageEmbed()
+                .setTitle('Ban Confirmation')
+                .setDescription(`Are you sure you want to ban ${target.username}?`)
+                .setColor('#FF0000');
+
+            const row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId('confirm_ban')
+                        .setLabel('Confirm')
+                        .setStyle('DANGER'),
+                    new MessageButton()
+                        .setCustomId('cancel_ban')
+                        .setLabel('Cancel')
+                        .setStyle('SECONDARY')
+                );
+
+            await interaction.reply({ embeds: [embed], components: [row] });
+
+            const filter = i => i.customId === 'confirm_ban' || i.customId === 'cancel_ban';
+            const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+            collector.on('collect', async i => {
+                if (i.customId === 'confirm_ban') {
+                    await member.ban();
+                    await i.update({ content: `${target.username} was banned.`, components: [] });
+                } else {
+                    await i.update({ content: 'Ban action canceled.', components: [] });
+                }
+            });
+
+            collector.on('end', collected => {
+                if (collected.size === 0) {
+                    interaction.editReply({ content: 'Ban action timed out.', components: [] });
+                }
+            });
+        } else {
+            await interaction.reply("You don't have the necessary permissions to use this command.");
+        }
     }
-
-    const member = interaction.options.getMember('target');
-    if (!member) return interaction.reply('You need to mention a user to ban.');
-
-    const row = new ActionRowBuilder()
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId('confirm-ban')
-          .setLabel('Confirm')
-          .setStyle(ButtonStyle.Danger),
-        new ButtonBuilder()
-          .setCustomId('cancel-ban')
-          .setLabel('Cancel')
-          .setStyle(ButtonStyle.Secondary),
-      );
-
-    await interaction.reply({
-      content: `Are you sure you want to ban ${member.user.tag}?`,
-      components: [row],
-    });
-
-    const filter = i => i.user.id === interaction.user.id;
-    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
-
-    collector.on('collect', async i => {
-      if (i.customId === 'confirm-ban') {
-        await member.ban();
-        await i.update({ content: `${member.user.tag} has been banned.`, components: [] });
-      } else {
-        await i.update({ content: 'Ban action cancelled.', components: [] });
-      }
-    });
-
-    collector.on('end', collected => {
-      if (collected.size === 0) {
-        interaction.editReply({ content: 'Ban action timed out.', components: [] });
-      }
-    });
-  }
 };
 
-      }
-    });
-  }
-};
